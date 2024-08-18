fb608aaca7404f2ea71f250ce610b5f97202ee8b
Vidhi <mathurvidhi2505@gmail.com>
2024-08-18 22:56:19 +0530

    Added basic intro and styling

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,3 +1,9 @@
+import "../../styles/specialeffects.css"
 export const HomePage = () => {
-    <h1>Home Page</h1>
+    return (
+        <div className="flex flex-col justify-start items-start min-h-screen text-left pl-20 pt-36">
+            <h1 className="text-9xl text-white font-bold mb-4 shimmer-effect">Hi! I'm Vidhi</h1>
+            <p className="text-5xl text-[#9374e8] opacity-80 animate-bounce-effect">A FULLSTACK DEVELOPER</p>
+        </div>
+    )
 }
