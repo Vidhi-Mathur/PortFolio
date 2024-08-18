f6ebdcefa27ab3109539a8b29b23bcb6b44e8d29
Vidhi <mathurvidhi2505@gmail.com>
2024-08-18 22:59:34 +0530

    Adjusted animation for bounce-effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,34 @@
 module.exports = {
 content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+            "bounce-effect": {
+                '0%, 100%': { 
+                    transform: 'translateY(0)'
+                },
+                '50%': {
+                    transform: 'translateY(-20px)'
+                }
+            }
+        },
+        animation: {
+            "bounce-effect": "bounce-effect 2s infinite"
+        }
+    },
   },
-  plugins: [],
+  plugins: [
+    function({addUtilities}) {
+        const newUtility = {
+            ".hide-scrollbar::-webkit-scrollbar": {
+                display: "none"
+            },
+            "hide-scrollbar": {
+                "-ms-overflow-style": "none",
+                "scrollbar-width": "none"
+            }
+        }
+        addUtilities(newUtility)
+    }
+  ],
 }
-
