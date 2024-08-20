fe1341598b88a0c8b6d4d4cf06767a9ef3f92d8c
Vidhi <mathurvidhi2505@gmail.com>
2024-08-20 12:18:31 +0530

    Contains skills and icons

---
+++ src/components/UI/Skills.js
@@ -0,0 +1,51 @@
+import HTML from "../../assets/skills/HTML.svg"
+import CSS from "../../assets/skills/CSS.svg"
+import Javascript from "../../assets/skills/Javascript.svg"
+import Bootstrap from "../../assets/skills/BootStrap.svg"
+import Java from "../../assets/skills/Java.svg"
+import ReactJS from "../../assets/skills/ReactJS.svg"
+import ReactRouterDom from "../../assets/skills/React-Router-Dom.svg"
+import Redux from "../../assets/skills/Redux.svg"
+import NextJS from "../../assets/skills/NextJS.svg"
+import MaterialUI from "../../assets/skills/Material-UI.svg"
+import TailwindCSS from "../../assets/skills/Tailwind-CSS.svg"
+import FramerMotion from "../../assets/skills/Framer-Motion.svg"
+import NodeJS from "../../assets/skills/NodeJS.svg"
+import ExpressJS from "../../assets/skills/ExpressJS.svg"
+import SocketIO from "../../assets/skills/SocketIO.svg"
+import MongoDB from "../../assets/skills/MongoDB.svg"
+import Git from "../../assets/skills/Git.svg"
+import Postman from "../../assets/skills/Postman.svg"
+
+export const langauageAndScriptingSkills = [
+        { name: 'HTML', icon: HTML },
+        { name: 'CSS', icon: CSS},
+        { name: 'Javascript', icon: Javascript},
+        { name: 'Java', icon: Java},
+]
+
+export const libraryAndFrameworkSkills = [
+        { name: 'Bootstrap', icon: Bootstrap},
+        { name: 'ReactJS', icon: ReactJS },
+        { name: 'React Router Dom', icon: ReactRouterDom },
+        { name: 'Redux', icon: Redux },
+        { name: 'NextJS', icon: NextJS },
+        { name: 'Material UI', icon: MaterialUI},
+        { name: 'Tailwind CSS', icon: TailwindCSS},
+        { name: 'Framer Motion', icon: FramerMotion },
+]
+
+export const backendSkills = [
+        { name: 'NodeJS', icon: NodeJS},
+        { name: 'ExpressJS', icon: ExpressJS },
+        { name: 'Socket.io', icon: SocketIO },
+]
+
+export const databaseSkills = [
+        { name: 'MongoDB', icon: MongoDB}
+]
+
+export const developerToolSkills = [
+        { name: 'Version Control', icon: Git},
+        { name: 'Postman', icon: Postman}
+]
